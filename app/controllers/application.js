d706309a23d1321b1e4d731b4bb7353148d51d8c
robgmerrill <robgmerrill@gmail.com>
2017-01-18 19:42:32 -0800

    create record in database

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,10 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     makeUnavailable(beer) {
-      Ember.set(beer, 'isAvailable', false)
+      Ember.set(beer, 'isAvailable', false);
+      beer.save();
     },
     makeAvailable(beer) {
       Ember.set(beer, 'isAvailable', true)
+      beer.save();
+    },
+    saveNewItem(newItem) {
+      this.store.createRecord('beer', {
+        isAvailable: false,
+        name: newItem
+      })
     }
   }
   });
