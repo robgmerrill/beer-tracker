98d33c66f0d426dec7ad9c1e5b74131bd030e5a2
robgmerrill <robgmerrill@gmail.com>
2017-01-19 16:14:38 -0800

    remove a beer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  newItem: null,
   actions: {
     makeUnavailable(beer) {
       Ember.set(beer, 'isAvailable', false);
@@ -10,11 +11,15 @@ export default Ember.Controller.extend({
       Ember.set(beer, 'isAvailable', true)
       beer.save();
     },
-    saveNewItem(newItem) {
+    saveNewItem() {
       this.store.createRecord('beer', {
         isAvailable: false,
-        name: newItem
-      })
+        name: this.get('newItem')
+      }).save()
+      Ember.set(this, 'newItem', '')
+    },
+    destroyItem(beer) {
+      beer.destroyRecord();
     }
   }
   });
